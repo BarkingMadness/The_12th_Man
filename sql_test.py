447bbb8778a0be663433a26659e2a23b49f9318f
BarkingMadness <andrewghirst@yahoo.co.uk>
2016-03-14 14:23:43 +0000

    Update sql_test.py

--- sql_test.py
+++ sql_test.py
@@ -2,20 +2,53 @@
 
 import sqlite3
 
-conn = sqlite3.connect('twelth_man.db')
+con = sqlite3.connect('twelth_man.db')
 print ("Opened database successfully")
 
 
-def create_db():
-	print("Creating fields")
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute("DROP IF TABLE EXISTS Contacts")
-		cursor.execute("""CREATE TABLE Contacts (Match INTEGER, Date TEXT, Opposition TEXT, Venue TEXT, Format TEXT""")
-		cursor.commit()
+class Database():
 
-def fixtures():
-	date = user_input("Date: ")
-	opp = user_input("Opposition: ")
-	venue = user_input("Venue: ")
-	format = user_input("Format: ")
+    def __init__(self):
+
+        self.date = ""
+        self.opposition = ""
+        self.venue = ""
+        self.format = ""
+
+
+    def input(self):
+        self.date = raw_input("Date of game: ")
+        self.opposition = raw_input("Opposition: ")
+        self.venue = raw_input("Venue: ")
+        self.format = raw_input("Format: ")
+        return self.date, self.opposition, self.venue, self.format
+
+
+    def create_db(self):
+        with con:
+            cur = con.cursor()
+            # cur.execute("DROP TABLE IF EXISTS Fixtures")
+            # cur.execute("CREATE TABLE Fixtures (First Name TEXT, Last Name TEXT, Phone TEXT, Email TEXT);")
+            cur.execute("INSERT INTO Fixtures VALUES (?, ?, ?, ?);", (self.date, self.opposition, self.venue, self.format))
+            con.commit()
+
+    def display_db(self):
+        print "\nHere's a listing of all the records in the table:\n"
+        with con:
+            cur = con.cursor()
+            cur.execute("SELECT * FROM Fixtures")
+            rows = cur.fetchall()
+            for row in rows:
+                print row
+
+
+    def main_loop(self):
+        fixture_input = self.input()
+        update_db = self.create_db()
+        view_db = self.display_db()
+
+
+if __name__ == '__main__':
+    input = Database()
+    input.main_loop()
+    con.close()

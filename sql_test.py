4ac14d0d4acfa7daa4ff63e115d84719b5a1b678
BarkingMadness <andrewghirst@yahoo.co.uk>
2016-03-15 15:10:48 +0000

    Update sql_test.py

--- sql_test.py
+++ sql_test.py
@@ -1,18 +1,22 @@
 #!/usr/local/bin/python2.7
 
 #import sqlite3 library to enable use of database
+
 import sqlite3
 
 # open the database and print message when it works. create a cursor object & table if it doesnt exist.
+
 con = sqlite3.connect('12thman_test.db')
 print ("Opened database successfully")
 cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS Fixtures (game_date TEXT, opposition TEXT, venue TEXT, formats TEXT);")
 
 # create a class called database to handle inputting, outputting and display of database information.
+
 class Database():
 
     # init class variables used to crreate fixture information.
+
     def __init__(self):
 
         self.game_date = ""
@@ -21,6 +25,7 @@ class Database():
         self.formats = ""
 
     # allows the user to make a choice from the menu
+
     def menu(self):
         print "\n------------------------"
         print "1. Enter Fixtures"
@@ -37,6 +42,7 @@ class Database():
             self.menu()
 
     # asks the user to input fixture details and return values
+
     def input_fixtures(self):
         self.game_date = raw_input("Date of game: ")
         self.opposition = raw_input("Opposition: ")
@@ -45,6 +51,7 @@ class Database():
         return self.game_date, self.opposition, self.venue, self.formats
 
     # take the user entered fixtures details and add them to the table 'Fixtures'. If one doesn't already exist, create it.
+
     def update_db(self):
         with con:
             cur = con.cursor()
@@ -53,17 +60,28 @@ class Database():
             con.commit()
 
     # displays all the fixtures currently in the database.
+
     def view_fixtures(self):
         print "\nList of Fixtures:\n"
         with con:
             cur = con.cursor()
-            cur.execute("SELECT * FROM Fixtures")
             cur.execute("SELECT * FROM Fixtures ORDER BY game_date ASC")
             rows = cur.fetchall()
             for row in rows:
                 print row
 
+        print "\nView match details? \n"
+        self.view_date = raw_input("Enter date: ")
+        with con:
+            cur = con.cursor()
+            cur.execute("select * from Fixtures where game_date = '%s'" % self.view_date)
+            for row in cur:
+                print row
+                con.commit()
+
+
     # main loop to run the methods in the Database class.
+
     def main_loop(self):
         self.menu()
         self.update_db()
